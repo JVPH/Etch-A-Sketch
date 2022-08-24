141b7001c014613cecc9eac3412652a446cf9d61
JVPH <gyrospizzeria@protonmail.com>
2022-08-23 21:51:35 -0500

    feat: make elements inside the grid change colors when hovered

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
 const gridContainer = document.querySelector('.grid-container');
 
-function setNumberOfGridElements(numberOfElements){
-    for (let i = 0; i < numberOfElements; i++) {
+function setGridDimensions(rows, columns){
+    for (let i = 0; i < rows*columns; i++) {
         let element = document.createElement('div');
         element.classList.add('grid-element');
+        element.setAttribute('style', 'border: thin solid #E1E5EA');
         gridContainer.appendChild(element);        
     }
 }
 
-setNumberOfGridElements(256);
+setGridDimensions(16,16);
+
+gridContainer.setAttribute('style', 'grid-template-rows: repeat(16, 1fr); grid-template-columns: repeat(16, 1fr);');
+
+
+const gridElements = document.querySelector('.grid-container').children;
+
+for (let i = 0; i < gridElements.length; i++) {
+   gridElements[i].addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+   });    
+}
