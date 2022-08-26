f580d4866d3b0facbbc08487263b535cc24912a6
JVPH <gyrospizzeria@protonmail.com>
2022-08-26 14:16:00 -0500

    feat: add rainbow mode option

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,33 @@ const sliderElement = document.querySelector('.grid-dimensions-slider');
 const sliderElementDisplayValue = document.querySelector('.slider-current-value');
 let gridElements = document.querySelector('.grid-container').children;
 const brushColorElement = document.querySelector('.brush-color');
+const rainbowModeButton = document.querySelector('.rainbow-mode-button');
+const clearButton = document.querySelector('.clear-button');
 
-// Set starting/default display
+// Set starting/default options
+
 sliderElementDisplayValue.textContent = `${sliderElement.value*16}X${sliderElement.value*16}`;
 
 fillGridContainer(sliderElement.value*16);
 
 gridContainerElement.setAttribute('style', ` grid-template-columns: repeat(16, 1fr);`);
 
-addEventListenerToChangeColorWhenHovered(brushColorElement.value);
+singleColorMode(brushColorElement.value);
 
+clearButton.addEventListener('click', clearGridColors);
+
+
+rainbowModeButton.addEventListener('click', rainbowMode);
 //------------------------------------
 
 brushColorElement.addEventListener('change', () => {
-    addEventListenerToChangeColorWhenHovered(brushColorElement.value);
+    singleColorMode(brushColorElement.value);
 });
 
 sliderElement.addEventListener('change', () => {
     fillGridContainer(sliderElement.value*16);
     setGridLayout();    
-    addEventListenerToChangeColorWhenHovered(brushColorElement.value);
+    singleColorMode(brushColorElement.value);
 });
 
 function setGridLayout() {    
@@ -48,10 +55,24 @@ function removeAllChildNodes(parent) {
     }
 }
 
-function addEventListenerToChangeColorWhenHovered(color){
+function singleColorMode(color){
     for (let i = 0; i < gridElements.length; i++) {
         gridElements[i].addEventListener('mouseover', (e) => {
-             e.target.style.backgroundColor = `${color}`;
+             e.target.style.backgroundColor = color;
         });    
     }
 }
+
+function rainbowMode(){
+    for (let i = 0; i < gridElements.length; i++) {
+        gridElements[i].addEventListener('mouseover', (e) => {
+             e.target.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        });    
+    }    
+}
+
+function clearGridColors(){
+    for (let i = 0; i < gridElements.length; i++) {
+       gridElements[i].setAttribute('style', 'background-color: white;');
+    }
+} 
